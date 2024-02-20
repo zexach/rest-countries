690600ade74b23372f0839caf5f9465c970c2882
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2024-02-20 15:11:14 +0100

    Added custom component for input

--- src/components/SearchCountry.vue
+++ src/components/SearchCountry.vue
@@ -4,8 +4,7 @@ import { ref } from 'vue';
 const emit = defineEmits(['onInput'])
 
 const emitValue = () => {
-    const searchValue = ref(searched)
-    emit('onInput', searchValue.value);
+    emit('onInput', searched.value);
 }
 
 const searched = ref('');
@@ -13,15 +12,22 @@ const searched = ref('');
 </script>
 
 <template>
-      <input class="search-bar" placeholder="Search for a country..." v-model="searched" @keypress="emitValue" type="text">
+  <div class="input">
+      <input class="search-bar" placeholder="Search for a country..." v-model="searched" type="text">
+      <button @click="emitValue">Search</button>
+  </div>
 </template>
 
 <style scoped>
-  .search-bar{
-    width: 35%;
-    height: 60px;
-    padding: 20px 20px;
+  .input{
     margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+    gap: 1rem
+  }
+  .search-bar{
+    width: 100%;
+    padding: 20px 20px;
     display: flex;
     align-items: center;
     border-radius: 7px;
@@ -34,5 +40,14 @@ const searched = ref('');
   }
   .search-bar:focus{
     outline: none
+  }
+  button{
+    padding: 20px;
+    background-color: white;
+    border: 0;
+    border-radius: 30px;
+    box-shadow:  0 6px 20px 0 rgba(161, 161, 161, 0.19);
+    cursor: pointer;
+
   }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,10 @@
   import { onMounted, ref, VueElement } from 'vue';
   import { computed } from '@vue/reactivity';
   import axios from 'axios';
-  import Country from '../components/Country.vue';
+  import CountryCard from '../components/CountryCard.vue';
   import FilterPanel from '../components/FilterPanel.vue';
   import Filter from '../components/Filter.vue';
+import SearchCountry from '../components/SearchCountry.vue';
 
   const showFilterPanel = ref(false)
   const searched = ref('')
@@ -15,7 +16,8 @@
   onMounted(async () => {
         getCountries('all')
   })
-
+  
+  const setSearchedValue = (val) => searched.value = val;
   const matchingResult = computed(() => {
     return countries.value.filter((name) => (name.name.common.toLowerCase()).includes(searched.value.toLowerCase()))
   })
@@ -36,25 +38,30 @@
       countries.value = response.data 
   }
 
+
 </script>
 
 <template>
+
   <div class="content">
     <div class="search-filter">
-      <input class="search-bar" placeholder="Search for a country..." v-model="searched" type="text">
+      <SearchCountry @onInput="setSearchedValue" />
       <div class="filter-container">
         <Filter @click="showFilterPanel = !showFilterPanel" :showFilterPanel="showFilterPanel" :filteredRegion="filteredRegion"  />
         <FilterPanel v-click-outside="clickOutside" @selectedRegion="handleFilter" :countries="countries" v-if="showFilterPanel" />
       </div>
     </div>
+
     <div class="countries">
       <div v-for="country in matchingResult" :key="country.capital" class="countries-container">
         <router-link :to="{ name: 'CountryDetails', params: { id: country.name.common } }">
-          <Country :country="country" />
+          <CountryCard :country="country" />
         </router-link>
       </div>
     </div>
+
   </div>
+
 </template>
 
 <style scoped>
